bcf43e937ad6c0133c5cdc0533da2afb241924f2
AndrusNikita <53003369+AndrusNikita@users.noreply.github.com>
2021-02-14 21:11:51 +0200

    Finish lab2

--- app/src/main/java/com/example/pmlabs/Lab2.java
+++ app/src/main/java/com/example/pmlabs/Lab2.java
@@ -2,6 +2,7 @@ package com.example.pmlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -23,12 +24,16 @@ public class Lab2 extends AppCompatActivity {
         EditText X = (EditText)findViewById(R.id.Xenter);
         TextView res = (TextView) findViewById(R.id.ResultText);
 
-        int x = Integer.parseInt(X.getText().toString());
+        double x = Double.parseDouble(X.getText().toString());
         int e = 5;
         int a = 4;
         int c = 8;
         double Res = (Math.sqrt((pow(e,x))-pow(Math.cos(pow(x,2) * pow(a,5)),4))+pow(atan(a-pow(x,5)),4)) / pow(abs(a+(x*pow(c,4))),-e);
-        res.setText(Double.toString(Res));
+        res.setText(String.format("%.2f", Res));
+    }
 
+    public void onInfoClick (View v){
+        Intent infoGo = new Intent(".AboutMe");
+        startActivity(infoGo);
     }
 }
